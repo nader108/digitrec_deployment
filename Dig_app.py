250853003671450d15502ff418fb19f0a2dd8bb5
Nader Nageh <nadernageh22508@gmail.com>
2025-01-20 15:48:36 +0200

    comit

---
+++ Dig_app.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import streamlit as st
+import numpy as np
+
+from PIL import Image
+
+Dig_model = tf.keras.models.load_model("dig_model.keras")
+
+
+st.title("MNIST Digit Recognizer")
+st.write("enter your photo")
+
+
+uploaded_file = st.file_uploader("load img", type=["png", "jpg", "jpeg"])
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file).convert("L")
+    st.image(image, caption="img", use_column_width=True)
+
+
+    if st.button("Predict"):
+
+        image_resized = image.resize((28, 28))
+        image_array = np.array(image_resized) / 255.0
+        image_array = image_array.reshape(1, 28, 28, 1)
+
+
+        predictions = Dig_model.predict(image_array)
+        predicted_digit = np.argmax(predictions)
+
+
+        st.success(f"num is : {predicted_digit}")
+        # st.write(f"احتمالات كل الأرقام: {predictions}")
+
+
